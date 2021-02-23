7328f9a8ef5057c6981bb941567c8dfff319794b
Germán <germanmacchia@gmail.com>
2021-02-23 00:28:22 -0300

    libros prestados

--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -11,6 +11,7 @@ export default function Nav () {
 					<li><Link to ={"/home"}><h1>Biblioteca</h1></Link></li>
 					<li><Link to ={"/categoria"}><h1>Categoria</h1></Link></li>
 					<li><Link to ={"/personas"}><h1>Personas</h1></Link></li>
+					<li ><a id="salir" href= "//localhost:3000/"><h1>Salir</h1></a></li>
 				</ul>
 			</nav> 
 			<div className='clear' />
