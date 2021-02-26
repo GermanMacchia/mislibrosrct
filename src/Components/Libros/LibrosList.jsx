af16362e06c7fcabd6f16fbf0263ee9bcdc6d3b5
Germán <germanmacchia@gmail.com>
2021-02-25 23:42:50 -0300

    icons y prestar

--- src/Components/Libros/LibrosList.jsx
+++ src/Components/Libros/LibrosList.jsx
@@ -3,9 +3,11 @@ import axios from 'axios';
 import EditarLibro from './EditarLibro'
 import { connect } from 'react-redux';
 import { useAlert } from 'react-alert';
+import Tooltip from '@material-ui/core/Tooltip';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
+import PersonIcon from '@material-ui/icons/Person';
 import VerticalAlignBottomIcon from '@material-ui/icons/VerticalAlignBottom';
 
 function LibrosList (props) {
@@ -35,15 +37,12 @@ function LibrosList (props) {
 					})
 					.catch((error) => {
 					  	console.error(error)
-					  	alert.error(`¿El libro esta prestado! No es posible borrarlo`)
-
+					  	alert.error(`No se puede borrar ¡El libro esta prestado!`)
 					});
 				}
 			}
 		deleteLibro ();
 	}
-
-	// EDITAR LIBRO
 
 	const handleEditar = (e) => {
 		e.preventDefault()
@@ -54,6 +53,35 @@ function LibrosList (props) {
 		editarLibro(e);
 	}
 
+	const handlePrestar = (e) =>{
+		e.preventDefault();
+		console.log(e.target.value)
+
+		const persona = prompt('INGRESA EL ID DE LA PERSONA:')
+
+		async function prestarLibro () {
+			await axios({
+				    method: 'put',
+				    url: `//localhost:8000/libro/prestar/` + e.target.value,
+				    headers: {'Authorization': props.state.AuthReducer[0].token},
+				    data: {
+				    	'id': e.target.value,
+				    	'persona_id': persona
+				    	}
+				    })
+				.then((res) => {
+					alert.success(`El libro se a prestado a la persona ${persona}`)
+					setReload(reload + 1 )
+
+				})
+				.catch((error) => {
+				  	console.error(error)
+				  	alert.error("La persona no existe o El libro ya esta prestado")
+				}
+		)}
+		prestarLibro ();
+	}
+
 	const handleDevolver = (e) => {
 		e.preventDefault()
 
@@ -61,7 +89,7 @@ function LibrosList (props) {
 				await axios({
 				    method: 'put',
 				    url: `//localhost:8000/libro/devolver/` + e.target.value,
-				    headers: {'Authorization': props.state.AuthReducer[0].token},
+				    headers: {'Authorization': props.state.AuthReducer[0].token}
 				    })
 				.then((res) => {
 					alert.success("El libro a vuelto a tu bibiblioteca")
@@ -104,7 +132,7 @@ function LibrosList (props) {
 	                <td id="categorialibro"><p>{libro.categoria_id}</p></td>    
 	                <td id="descripcionlibro"><p>{libro.descripcion}</p></td>
 	            	<td id="personalibro"><p>{libro.persona_id}</p></td>
-	            	<td id= "prestarbtt"><button className="funcionBtt" value= {libro.id}>P</button></td>
+	            	<td id= "prestarbtt"><button className="funcionBtt" onClick={handlePrestar} value= {libro.id}>P</button></td>
 	            	<td id= "devolverbtt"><button className="funcionBtt" onClick={handleDevolver} value= {libro.id}>↕</button></td>
 	            	<td id= "borrarbtt"><button className="funcionBtt" onClick={handleDelete} value= {libro.id}>X</button></td>
 					<td id= "editarbtt"><button  className="funcionBtt" onClick={handleEditar} value= {libro.id}>E</button></td>
@@ -126,13 +154,31 @@ function LibrosList (props) {
 	                    <th>Nombre</th>
 	                    <th>Categoría</th>
 	                    <th id='descripcion_titulo'>Descripción</th>
-	                    <th>Persona
-	                    	-ID-</th>
-	                    <th className="funcion tooltip"><MenuBookIcon /></th>
-	                    <th className="funcion tooltip"><div className="tooltiptext">Devolver</div><VerticalAlignBottomIcon /></th>
-	                    <th className="funcion"><DeleteIcon /></th>
-						<th className="funcion"><EditIcon /></th>
-	                    
+	                    <th>
+	                    	<Tooltip title= "Persona ID">
+	                    		<PersonIcon />
+	                    	</Tooltip>
+	                    </th>
+	                    <th className="funcion">
+	                    	<Tooltip title= "Prestar">
+	                    		<MenuBookIcon />
+	                    	</Tooltip>
+	                    </th>
+	                    <th className="funcion">
+	                    	<Tooltip title= "Devolver">
+	                    		<VerticalAlignBottomIcon />
+	                    	</Tooltip>
+	                    </th>
+	                    <th className="funcion">
+	                    	<Tooltip title= "Borrar">
+	                    		<DeleteIcon />
+	                    	</Tooltip>
+	                    </th>
+						<th className="funcion">
+							<Tooltip title= "Editar">
+	                    		<EditIcon />
+	                    	</Tooltip>
+	                    </th>            
 	                </tr>
                 </thead>
 	            <tbody>
