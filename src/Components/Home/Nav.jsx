b48cefb01b635bff5e623dd6330ff6682ef59d59
Germán <germanmacchia@gmail.com>
2021-02-27 01:32:37 -0300

    diseño

--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import CategoryIcon from '@material-ui/icons/Category';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { green, purple } from '@material-ui/core/colors';
 
 export default function Nav () {
 
@@ -7,14 +13,58 @@ export default function Nav () {
 	return(
 		<>
 			<nav id="nav">
-				<ul>
-					<li><Link to ={"/home"}><h1>Biblioteca</h1></Link></li>
-					<li><Link to ={"/categoria"}><h1>Categoria</h1></Link></li>
-					<li><Link to ={"/personas"}><h1>Personas</h1></Link></li>
-					<li ><a id="salir" href= "//localhost:3000/"><h1>Salir</h1></a></li>
+				<ul >
+				<div className="buttonsNav">
+					<li>
+						<Link to ={"/home"}>
+							<Button
+								className="butNav"
+						        variant="contained"
+						        color="green"
+						        startIcon={<LocalLibraryIcon />}>
+						        Biblioteca
+						    </Button>
+						</Link>
+				    </li>
+					<li>
+						<Link to ={"/categoria"}>
+							<Button
+								className="butNav"
+						        variant="contained"
+						        color="default"
+						        startIcon={<CategoryIcon />}>
+						        Categoria
+						    </Button>
+						</Link>
+					</li>
+					</div>
+					<div className="buttonsNav">
+					<li>
+						<Link to ={"/personas"}>
+							<Button
+								className="butNav"
+						        variant="contained"
+						        color="default"
+						        startIcon={<PersonPinIcon />}>
+						        Personas
+						    </Button>
+						</Link>
+					</li>
+					<li >
+						<a id="salir" href= "//localhost:3000/">
+							<Button
+								className="butNav"
+						        variant="contained"
+						        color="default"
+						        startIcon={<ExitToAppIcon />}>
+						        Salir
+						    </Button>
+						</a>
+					</li>
+				</div>
 				</ul>
 			</nav> 
-			<div className='clear' />
+
 			
 		</>
 	);
